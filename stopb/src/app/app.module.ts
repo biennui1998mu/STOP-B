ac1950d69c32d5692d9229118fbc215a2130852f
Nguyen Son <valueislife@gmail.com>
2020-04-05 22:29:26 +0700

    update

--- stopb/src/app/app.module.ts
+++ stopb/src/app/app.module.ts
@@ -7,14 +7,14 @@ import { HomepageComponent } from './core/homepage/homepage.component';
 import { QnoteComponent } from './core/qnote/qnote.component';
 import { MakePlanComponent } from './core/makeplan/make-plan.component';
 import { ReadnoteComponent } from './readNote/readnote.component';
-import { ReadplanComponent } from './readProject/readProject.component';
+import { ReadProjectComponent } from './readProject/readProject.component';
 import { SharedComponentModule } from "./shared/components/shared-component.module";
 import { CoreImportsModule } from './shared/modules/core-imports.module';
 import { SharedServicesModule } from './shared/services/shared-services.module';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AuthenticateGuard } from "./shared/guard/authenticate.guard";
-
+import { SocketService } from "../app/services/socket.service"
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import { AuthenticateGuard } from "./shared/guard/authenticate.guard";
     QnoteComponent,
     MakePlanComponent,
     ReadnoteComponent,
-    ReadplanComponent,
+    ReadProjectComponent,
   ],
   imports: [
     CoreImportsModule,
@@ -36,6 +36,7 @@ import { AuthenticateGuard } from "./shared/guard/authenticate.guard";
   ],
   providers: [
     AuthenticateGuard,
+    SocketService
   ],
   bootstrap: [AppComponent],
 })
--- stopb/src/app/services/socket.service.ts
+++ stopb/src/app/services/socket.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 import { Observable } from 'rxjs';
 import { User } from "../shared/interface/User";
+import { environment } from 'src/environments/environment';
+import {TokenService} from "./token.service";
 
 @Injectable({
   providedIn: 'root',
@@ -13,21 +15,17 @@ export class SocketService {
   private url = 'http://localhost:3000';
   private socket;
 
-  constructor() {
-    this.socket = io(this.url);
+  constructor(
+    private tokenService: TokenService
+  ) {
   }
 
-
-  public sendMessage(message) {
-    this.socket.emit('send-message', message);
-  }
-
-  public getMessages = () => {
-    return Observable.create((observer) => {
-      this.socket.on('receive-message', function (message) {
-        observer.next(message);
-      });
+  setupSocketConnection() {
+    this.socket = io(this.url, {
+      query: {
+        token: this.tokenService.getToken()
+      }
     });
-  };
+  }
 
 }
